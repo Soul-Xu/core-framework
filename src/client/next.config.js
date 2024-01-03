30c38d8faed293dda57389caaddc90d08dab3253
leonxu0207 <741930067@qq.com>
2024-01-03 14:44:12 +0800

    feat: 修改接口端口号8080为80

--- src/client/next.config.js
+++ src/client/next.config.js
@@ -1,17 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  // devServer: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://120.79.58.103:8080/",
-  //       changeOrigin: true,
-  //       pathRewrite: {
-  //         "^/api": "http://120.79.58.103:8080/api/sys-auth/ ",
-  //       },
-  //     },
-  //   },
-  // },
   webpack: (config, { buildId, dev, isServer, defaultLoaders }) => {  
     config.resolve.alias['components'] = path.resolve(__dirname, "./components")
     return config;
--- src/server/config/index.ts
+++ src/server/config/index.ts
@@ -1,5 +1,5 @@
-export const baseApi = "http://120.79.58.103:8080/api/sys-auth"
-export const baseApiOrg = "http://120.79.58.103:8080/api/sys-org"
+export const baseApi = "http://120.79.58.103:80/api/sys-auth"
+export const baseApiOrg = "http://120.79.58.103:80/api/sys-org"
 export const Global = {
   token: ""
 }
